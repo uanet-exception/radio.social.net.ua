509fe83716709c138ec79837a1c0f453d31dc15f
ZEN <dmytro.poltavchenko@gmail.com>
2023-07-08 17:40:09 +0300

    Try to restart stream after 5 second

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -1,68 +1,61 @@
 const RadioPlayer = ((STREAM_URL) => {
-    let stream = new Audio();
-    stream.src = `${STREAM_URL}?nocache=${Date.now()}`;
-    stream.volume = 0.51;
-    stream.preload = 'none';
-    let updateInterval;
+    const playbtn = document.getElementById("play-toggle");
+    const volumebtn = document.getElementById("volume-button");
 
-    function updatePlayButton() {
-        const button = document.getElementById("play-toggle");
-        if(stream.paused) {
-            button.classList.remove('fa-pause');
-            button.classList.add('fa-play');
-        } else {
-            button.classList.remove('fa-play');
-            button.classList.add('fa-pause');
-        }
+    let stream = new Audio();
+    stream.src = `${STREAM_URL}`;
+    stream.volume = 1;
+    stream.preload = 'none';
+    stream.onended = function() {
+        console.log("onended");
+        stream.pause();
+        stream.src = '';
+        stream.load();
+    };
+
+    stream.onerror = function(e) {
+        if (e.target.error.code !== e.target.error.MEDIA_ERR_NETWORK && stream.src !== '')
+            return;
+
+        console.log('Network interrupted stream. Automatically reconnecting shortly...');
+        setTimeout(() => {
+            if (playbtn.classList.contains('fa-play')) {
+                return;
+            }
+            play();
+        }, 5000);
+    };
+
+    function play() {
+        console.log("play");
+        stream.src = `${STREAM_URL}?nocache=${Date.now()}`;
+        stream.preload = 'none';
+        stream.play();
+        playbtn.className = 'fa fa-pause';
+    }
+
+    function pause() {
+      console.log("pause");
+      stream.pause();
+      playbtn.className = 'fa fa-play';
     }
 
     function togglePlay() {
-        if(stream.paused) {
-            stream.play();
-        } else {
-            stream.pause();
-            stream.src = '';
-            stream.load();
-
-            var volume = stream.volume;
-
-            stream = null;
-            stream = new Audio();
-            stream.src = `${STREAM_URL}?nocache=${Date.now()}`;
-            stream.preload = 'none';
-            stream.volume = volume;
-            stream.pause();
-        }
-        updatePlayButton();
-    }
-
-    function updateVolumeButton() {
-        // Updates the volume button icon accoring to the volume
-        volumebtn = document.getElementById("volume-button");
-        if (stream.volume === 0) {
-            volumebtn.classList.remove('fa-volume-up');
-            volumebtn.classList.remove('fa-volume-down');
-            volumebtn.classList.add('fa-volume-off');
-        } else if (stream.volume > 0.51) {
-            volumebtn.classList.remove('fa-volume-down');
-            volumebtn.classList.remove('fa-volume-off');
-            volumebtn.classList.add('fa-volume-up');
-        } else {
-            volumebtn.classList.remove('fa-volume-up');
-            volumebtn.classList.remove('fa-volume-off');
-            volumebtn.classList.add('fa-volume-down');
-        }
+        playbtn.classList.contains('fa-play') ? play() : pause();
     }
 
     function cycleVolume() {
-        if (stream.volume < 0.51) {
-            stream.volume = 0.51;
-        } else if (stream.volume < 1) {
+        // if (stream.volume < 0.51) {
+        //     stream.volume = 0.51;
+        //     volumebtn.className = 'fa fa-volume-down';
+        // } else
+        if (stream.volume < 1) {
             stream.volume = 1;
+            volumebtn.className = 'fa fa-volume-up';
         } else {
             stream.volume = 0;
+            volumebtn.className = 'fa fa-volume-off';
         }
-        updateVolumeButton();
     }
 
     return {
