0e30f81eeeb844aa70d3f74ad7cb31dc4d5cab3d
ZEN <dmytro.poltavchenko@gmail.com>
2023-07-12 01:45:17 +0300

    Remove hardcoded playlists

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models import IcecastStatus
-from flask import render_template, jsonify
+from flask import render_template, make_response, jsonify
 
 
 @app.route('/')
@@ -8,8 +8,22 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/live.mp3.m3u')
+def live_m3u():
+    resp = make_response(render_template('live.m3u'), 200)
+    resp.mimetype = 'application/x-mpegURL'
+    return resp
+
+
+@app.route('/live.mp3.xspf')
+def live_xspf():
+    resp = make_response(render_template('live.xspf'), 200)
+    resp.mimetype = 'application/xspf+xml'
+    return resp
+
+
 @app.route('/playlist.json')
-def playlist():
+def playlist_json():
     icestats = IcecastStatus(app.config['STATUS_HISTORY_FILE'])
     icestats.load()
     return jsonify({
--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config(object):
     SITE_DESCRIPTION = os.environ.get('SITE_DESCRIPTION', 'Our selection of music')  # NOQA: E501
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
     ICECAST_STREAM_URL = os.environ.get('ICECAST_STREAM_URL', 'http://radio.social.net.ua/live.mp3')  # NOQA: E501
+    ICECAST_STREAM_BITRATE = os.environ.get('ICECAST_STREAM_BITRATE', '128')  # NOQA: E501
     ICECAST_STATUS_JSON_URL = os.environ.get('ICECAST_STATUS_JSON_URL', 'http://radio.social.net.ua/status-json.xsl')  # NOQA: E501
     STATUS_HISTORY_FILE = os.environ.get('STATUS_HISTORY_FILE', os.path.join(basedir, 'icestats_history.json'))  # NOQA: E501
     FEDIVERSE_URL = os.environ.get('FEDIVERSE_URL', 'https://social.net.ua')
